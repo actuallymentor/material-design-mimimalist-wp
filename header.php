10065f20f24d5a5929e2fd7ee007317bf7ea88e6
Mentor <mentor@palokaj.co>
2016-05-13 11:21:00 +0200

    Nav under header

--- header.php
+++ header.php
@@ -8,19 +8,35 @@
 </head>
 <body <?php body_class(); ?>>
 	<header id="header" role="banner">
-	<!-- Header block -->
+		<!-- Header block -->
 
-	<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ): ?>
+
 		<!--Header intro block code-->
 		<div id="header-intro" class="">
 			<div class="col l12 s12 valign-wrapper">
 				<div class="container center">
 					<h1 id="title" class="valign white-text center-align col l12 m12 s12"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
-					<h5 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php bloginfo( 'description' ); ?></h5>
+
+					<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ): ?>
+						<h2 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php bloginfo( 'description' ); ?></h2>
+
+					<?php elseif ( is_single (  )  ): ?>
+						<h2 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php the_title(); ?></h2>
+					<?php endif; ?>
+
 				</div>
 			</div>
 		</div>
-	<?php endif; ?>
 
-</header>
-<main class="row" id="container">
+		<div class="navbar">
+			<nav id="main-nav">
+				<div class="nav-wrapper">
+					<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+				</div>
+			</nav>
+		</div>
+
+
+
+	</header>
+	<main class="row" id="container">
