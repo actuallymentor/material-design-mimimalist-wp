89d8aa6d760de7b39c9219d6bb82ce9cc08eb8b5
Mentor <mentor@palokaj.co>
2016-05-13 14:35:52 +0200

    Better title style with truncate

--- index.php
+++ index.php
@@ -22,62 +22,47 @@
 		<!-- Filter style based on post number -->
 		<!-- First article -->
 		<?php if ( $article == 0 ) : ?> 
-			<article class="col l12 m12 s12 <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
+			<article class="featured col l12 m12 s12 <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
 				<header class="card">
 					<div class="card-image black">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
 							<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'large' ); } ?>
 						</a>
-						<a class="entry-title card-title white-text" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-					</div>
-					<div class="card-action">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"> 
-							Read this...
-						</a>
+						<span class="entry-title card-title white-text" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></span>
 					</div>
 				</header>
 			</article>
 
 
 		<?php elseif ( $article < 5 ) : ?> 
-			<article class="col l6 m6 s12 <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
+			<article class="medium col l6 m6 s12 <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
 				<header class="card">
 
 					<div class="card-image black">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
 							<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
 						</a>
-						<a class="entry-title card-title white-text" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-					</div>
-					<div class="card-action">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"> 
-							Read this...
-						</a>
+						<a class="truncate entry-title card-title white-text" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
 					</div>
 				</header>
 			</article>
 
 
 		<?php elseif ( $article < 9 ) : ?> 
-			<article class="col l3 m6 s12 <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
+			<article class="small col l3 m6 s12 <?php post_class(); ?>" id="post-<?php the_ID(); ?>">
 				<header class="card">
 					<div class="card-image black">
 						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
 							<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'small' ); } ?>
 						</a>
-						<a class="entry-title card-title white-text" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-					</div>
-					<div class="card-action">
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"> 
-							Read this...
-						</a>
+						<a class="truncate entry-title card-title white-text" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
 					</div>
 				</header>
 			</article>
 
 		<?php else : ?> 
 			<?php if  ( $article == 9 ): ?> 
-				<div class="col l12 m12 s12 collection">
+				<div class="col l12 m12 s12 card collection">
 				<? endif; ?>
 				<a class="collection-item" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"> 
 					Read: <?php the_title();  ?>
@@ -87,7 +72,7 @@
 			<?php $article++ ; // Increment post number ?>
 		<?php endwhile; endif; ?>
 	</div>
-	
+
 </section>
 
 
