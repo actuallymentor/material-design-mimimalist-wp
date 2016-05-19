27fb70d415712b20a23582c8d0e8797f7905c20d
Mentor <mentor@palokaj.co>
2016-05-19 18:06:44 +0200

    Flow titles so pretty!

--- header.php
+++ header.php
@@ -47,18 +47,18 @@
 
 							<!-- Home page -->
 							<?php if ( is_front_page() || is_home() || is_front_page() && is_home() ): ?>
-								<a id="title" class="valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
+								<a id="title" class="flow-text valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
 									<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
 								</a>
-								<h2 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php bloginfo( 'description' ); ?></h2>
+								<h2 id="subtitle" class="flow-text valign white-text center-align col l12 m12 s12"><?php bloginfo( 'description' ); ?></h2>
 
 								<!-- Blog page stuff -->
 							<?php elseif ( is_single (  )   ): ?>
-								<a id="title" class="valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
+								<a id="title" class="flow-text valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
 									<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 								</a>
 								<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-									<h1 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php the_title(); ?></h1>
+									<h1 id="subtitle" class="flow-text valign white-text center-align col l12 m12 s12"><?php the_title(); ?></h1>
 								</a>
 								<div class="col l12 m12 s12 meta white-text center">
 									<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -69,31 +69,31 @@
 
 								<!-- Archives page stuff -->
 							<?php elseif ( is_archive (  )   ): ?>
-								<a id="title" class="valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
+								<a id="title" class="flow-text valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
 									<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 								</a>
 								<header>
 									<a href="<?php get_permalink(); ?>" title="<?php the_archive_title(); ?>">
-										<h1 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php the_archive_title( 'Posts with ' ); ?></h1>
+										<h1 id="subtitle" class="flow-text valign white-text center-align col l12 m12 s12"><?php the_archive_title( 'Posts with ' ); ?></h1>
 									</a>
 								</header>
 
 								<!-- Archives page stuff -->
 							<?php elseif ( is_404 (  )   ): ?>
-								<a id="title" class="valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
+								<a id="title" class="flow-text valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
 									<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
 								</a>
 								<header>
-									<h2 id="subtitle" class="valign white-text center-align col l12 m12 s12">Oh no! Page not found!</h2>
+									<h2 id="subtitle" class="flow-text valign white-text center-align col l12 m12 s12">Oh no! Page not found!</h2>
 								</header>
 
 								<!-- Archives page stuff -->
 							<?php elseif ( is_search (  )   ): ?>
-								<a id="title" class="valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
+								<a id="title" class="flow-text valign white-text center-align col l12 m12 s12" href="<?php echo get_option('siteurl') ?>">
 									<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
 								</a>
 								<header>
-									<h2 id="subtitle" class="valign white-text center-align col l12 m12 s12"><?php printf( __( 'Search Results for: %s', 'materialize' ), get_search_query() ); ?></h2>
+									<h2 id="subtitle" class="flow-text valign white-text center-align col l12 m12 s12"><?php printf( __( 'Search Results for: %s', 'materialize' ), get_search_query() ); ?></h2>
 								</header>
 
 
