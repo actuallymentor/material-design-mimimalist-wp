73f393a93284a7945137720243f67557bfcea5ea
Mentor <mentor@palokaj.co>
2016-05-25 15:54:43 +0200

    reverse

--- js/custom.js
+++ js/custom.js
@@ -1,85 +1,80 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	if  ( $ == undefined ) {
-		var js = document.createElement("script")
-		js.type = "text/javascript"
-		js.src = "<?php echo get_template_directory_uri(); ?>/js/jquery-2.2.3.min.js"
-		document.body.appendChild(js)
+// Function declarations
+
+
+// Fading in the dropdown menu
+var menufadein = function (  ) {
+	console.log ( 'Hovered child containing menu' ) 
+	$ ( '.sub-menu li', this ).fadeIn ( 100, function (  ) {
+		$ ( this ).addClass ( 'z-depth-1' )  
+	}  )  
+}
+
+// Fading out the dropdown menu
+var menufadeout = function (  ) {
+	$ ( '.sub-menu li', this ).removeClass ( 'z-depth-1' )  
+	$ ( '.sub-menu li', this ).fadeOut ( 200 ) 
+}
+
+// Depth controllers
+
+var depthadd = function ( ) {
+	$ ( this ).addClass ( 'z-depth-2' )
+}
+var depthrem = function (  ) {
+	$ ( this ).removeClass ( 'z-depth-2' )
+}
+
+var lazyblock = function ( context )  {
+	var imagelist = $ ( 'img', context)
+	for  ( var i = 0; i < imagelist.length; i++ ) {
+		$ ( imagelist[i] ).attr ( 'lazy', $ ( imagelist[i] ).attr ( 'src' )   )
+		$ ( imagelist[i] ).attr ( 'lazyset', $ ( imagelist[i] ).attr ( 'srcset' )   )
+		$ ( imagelist[i] ).attr ( 'src', '' ) 
+		$ ( imagelist[i] ).attr ( 'srcset', '' ) 
+	}	
+}
+var lazyload = function  (  )  {
+	if  ( $ ( this ).attr ( 'src' ).length < 1   ) {
+		console.log ( 'Lazyload triggered' ) 
+		$ ( this ).attr ( 'src', $ ( this ).attr ( 'lazy' )   )
+		$ ( this ).attr ( 'srcset', $ ( this ).attr ( 'lazyset' )   )
+		$ ( this ).attr ( 'lazy', '' ) 
+		$ ( this ).attr ( 'lazysrc', '' ) 
 	}
-	// Fading in the dropdown menu
-	var menufadein = function (  ) {
-		console.log ( 'Hovered child containing menu' ) 
-		$ ( '.sub-menu li', this ).fadeIn ( 100, function (  ) {
-			$ ( this ).addClass ( 'z-depth-1' )  
-		}  )  
-	}
-
-	// Fading out the dropdown menu
-	var menufadeout = function (  ) {
-		$ ( '.sub-menu li', this ).removeClass ( 'z-depth-1' )  
-		$ ( '.sub-menu li', this ).fadeOut ( 200 ) 
-	}
-
-	// Depth controllers
-
-	var depthadd = function ( ) {
-		$ ( this ).addClass ( 'z-depth-2' )
-	}
-	var depthrem = function (  ) {
-		$ ( this ).removeClass ( 'z-depth-2' )
-	}
-
-	var lazyblock = function ( context )  {
-		var imagelist = $ ( 'img', context)
-		for  ( var i = 0; i < imagelist.length; i++ ) {
-			$ ( imagelist[i] ).attr ( 'lazy', $ ( imagelist[i] ).attr ( 'src' )   )
-			$ ( imagelist[i] ).attr ( 'lazyset', $ ( imagelist[i] ).attr ( 'srcset' )   )
-			$ ( imagelist[i] ).attr ( 'src', '' ) 
-			$ ( imagelist[i] ).attr ( 'srcset', '' ) 
-		}	
-	}
-	var lazyload = function  (  )  {
-		if  ( $ ( this ).attr ( 'src' ).length < 1   ) {
-			console.log ( 'Lazyload triggered' ) 
-			$ ( this ).attr ( 'src', $ ( this ).attr ( 'lazy' )   )
-			$ ( this ).attr ( 'srcset', $ ( this ).attr ( 'lazyset' )   )
-			$ ( this ).attr ( 'lazy', '' ) 
-			$ ( this ).attr ( 'lazysrc', '' ) 
-		}
-	}
+}
 
 
-	// jQuery actions after DOM loading is complete
-	$ ( document ).on ( 'ready', function (  ) {
-		console.log ( 'DOM ready' ) 
+// jQuery actions after DOM loading is complete
+$ ( document ).on ( 'ready', function (  ) {
+	console.log ( 'DOM ready' ) 
 
-		$.getScript ( 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js', function (  ) {
-			console.log ( 'Materialize loaded' ) 
-			// Lazyload implementation
-			//Lazyblock
-			lazyblock ( '#article' ) 
-			//Lazyload
-			$('img', '#article').scrollSpy (  )
-			$('img', '#article').on ( 'scrollSpy:enter', lazyload ) 
-
-
-			// Initialize mobile nav
-			$("#button-collapse").sideNav()
-
-			// Trigger fading on submenu items
-			$ ( '.menu-item-has-children', '#desktopmenu' ).hover ( menufadein, menufadeout)
-			$ ( '.menu-item-has-children .sub-menu li', '#desktopmenu' ).hover ( depthadd, depthrem  )
-
-			// Theme box hovering
-			$ ( '.box', '#content' ).hover ( depthadd, depthrem  )
-
-			// Make buttons pretty
-			$ ( 'input[type=submit]', '#content' ).addClass ( 'btn' )  
+	$.getScript ( 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js', function (  ) {
+		console.log ( 'Materialize loaded' ) 
+		// Lazyload implementation
+		//Lazyblock
+		lazyblock ( '#article' ) 
+		//Lazyload
+		$('img', '#article').scrollSpy (  )
+		$('img', '#article').on ( 'scrollSpy:enter', lazyload ) 
 
 
-		} ) 
+		// Initialize mobile nav
+		$("#button-collapse").sideNav()
+		
+		// Trigger fading on submenu items
+		$ ( '.menu-item-has-children', '#desktopmenu' ).hover ( menufadein, menufadeout)
+		$ ( '.menu-item-has-children .sub-menu li', '#desktopmenu' ).hover ( depthadd, depthrem  )
+
+		// Theme box hovering
+		$ ( '.box', '#content' ).hover ( depthadd, depthrem  )
+
+		// Make buttons pretty
+		$ ( 'input[type=submit]', '#content' ).addClass ( 'btn' )  
+
+
+	} ) 
 
 
 
-		// End of menu hovering  
-	} )  
-})
+	// End of menu hovering  
+} )  
