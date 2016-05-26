29e6deecfcbdfb0702ec976b4572258b88488553
Mentor <mentor@palokaj.co>
2016-05-26 14:07:11 +0200

    Analytics added

--- js/custom.js
+++ js/custom.js
@@ -71,10 +71,31 @@ $ ( document ).on ( 'ready', function (  ) {
 		// Make buttons pretty
 		$ ( 'input[type=submit]', '#content' ).addClass ( 'btn' )  
 
-
 	} ) 
 
+	// GA Event tracking
+	$('#wprm_submit').on('click', function() {
+			ga('send', 'event', {
+				eventCategory: 'Benefits',
+				eventAction: 'Signup',
+				eventLabel: 'Remindme'
+			})
+		});
+		$("#mdsh_lin, #mdsh_twi, #mdsh_fb").on('click', function() {
+			ga('send', 'event', {
+				eventCategory: 'Social sharing',
+				eventAction: 'Button',
+				eventLabel: 'Click'
+			})
+		});
+		$('#wpsp_submit').on('click', function() {
+			ga('send', 'event', {
+				eventCategory: 'Benefits',
+				eventAction: 'Signup',
+				eventLabel: 'Headerwell'
+			})
+		});
 
 
-	// End of menu hovering  
+	
 } )  
