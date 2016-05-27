8bbd429ceb11e1b0d7b79fcecd907f654e626df8
Mentor <mentor@palokaj.co>
2016-05-27 20:14:19 +0200

    Extra comments

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ function analytics_tags() { ?>
 	<!-- Google Analytics -->
 	<script>
 		document.addEventListener("DOMContentLoaded", function(event) { 
-		// GA
+		// GA Tracking code
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -190,7 +190,7 @@ function analytics_tags() { ?>
 			ga('require', 'displayfeatures');
 			ga('send', 'pageview');
 
-		// FB
+		// FB traxking pixel
 			!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 				n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
 				n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
@@ -201,9 +201,10 @@ function analytics_tags() { ?>
 				fbq('track', 'PageView');
 			})
 	</script>
-	<noscript><img height="1" width="1" style="display:none"
+	<noscript>
+		<img height="1" width="1" style="display:none"
 		src="https://www.facebook.com/tr?id=500924636726230&ev=PageView&noscript=1"
-		/></noscript>
-		<!-- End Facebook Pixel Code -->
+		/>
+	</noscript>
 		<?php
 	}
