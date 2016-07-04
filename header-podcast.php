2ac1da9aeb23453c0c585d3ae1320ab52644a4c7
Mentor <mentor@palokaj.co>
2016-07-04 21:06:17 +0200

    Link function fix

--- header-podcast.php
+++ header-podcast.php
@@ -1,5 +1,5 @@
-<a href="<?php echo get_permalink(); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?> Podcast">
+<a href="<?php echo get_post_type_archive_link( 'podcast' ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?> Podcast">
 <h1 id="subtitle" class="flow-text valign white-text center-align col l12 m12 s12">
-		<?php echo get_option('siteurl') ?> Podcast
+		<?php echo esc_html( get_bloginfo( 'name' ) ); ?> Podcast
 	</h1>
 </a>
